2509118b69f2dc60f084ff0859fabe59f05de258
程增 <xyzchz@outlook.com>
2020-11-02 18:52:06 +0800

    V1-5

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   // JavaScript 执行入口文件
@@ -8,5 +9,27 @@ module.exports = {
     filename: 'bundle.js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback:{
+            loader:'style-loader',
+            options:{
+              //insert:"body",
+            }
+          },
+          use: ['css-loader'],
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      // 从 .js 文件中提取出来的 .css 文件的名称
+      filename: `[name]_[contenthash:8].css`,
+    })
+  ]
 }
