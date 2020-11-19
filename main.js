36ee58555aadd0fe073340f96d48ff4ae3122c85
程增 <xyzchz@outlook.com>
2020-11-19 17:31:37 +0800

    devserver配置及 sourcemap

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ require('./main.css');
 // 通过 CommonJS 规范导入 show 函数
 const show = require('./show.js');
 // 执行 show 函数
-show('Webpack');
+show('hha');
+
+console.log('测试热更新1')
+
+const a = 123
+
+a.map(item => item)
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,19 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   // JavaScript 执行入口文件
   entry: './main.js',
+  // context: path.resolve(__dirname, ''),
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: 'bundle.js',
+    filename: '[name]234.js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
   },
+  devServer: {
+    port: '8099',
+    // progress: true,
+    hot: true,
+  },
+  devtool: "source-map", // sourceMap 查错
   module: {
     rules: [
       {
